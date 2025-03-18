8e2c62e5e258d8e786c87f19acb0e6a75238873f
Simon Walker <simon.walker@localstack.cloud>
2025-03-18 12:25:56 +0000

    Extract configureEnvironment and update implementation
    * sanitise environment
    * handle endpoint url configuration misconfiguration
    * add testing

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,59 @@
-describe("Simple test", () => {
-  test("it passes", () => {
-    expect(true).toEqual(true);
+const { configureEnvironment, EnvironmentMisconfigurationError } = require("../src");
+
+describe("configureEnvironment", () => {
+  test("empty environment", () => {
+    const env = {};
+    const allowListStr = "";
+    configureEnvironment(env, allowListStr);
+    expect(env).toEqual({
+      AWS_ENDPOINT_URL: "http://localhost.localstack.cloud:4566",
+      AWS_ENDPOINT_URL_S3: "http://s3.localhost.localstack.cloud:4566",
+    });
+  });
+
+  test("custom endpoint url", () => {
+    const env = {
+      AWS_ENDPOINT_URL: "http://foo.bar:4567",
+      AWS_ENDPOINT_URL_S3: "http://foo.bar:4567",
+    };
+    const allowListStr = "";
+    configureEnvironment(env, allowListStr);
+    expect(env).toEqual({
+      AWS_ENDPOINT_URL: "http://foo.bar:4567",
+      AWS_ENDPOINT_URL_S3: "http://foo.bar:4567",
+    });
+  });
+
+  test("custom endpoint url without specifying s3 url", () => {
+    const env = {
+      AWS_ENDPOINT_URL: "http://foo.bar:4567",
+    };
+    const allowListStr = "";
+    expect(() => configureEnvironment(env, allowListStr)).toThrow(EnvironmentMisconfigurationError);
+  });
+
+  test("strip extra configuration envars", () => {
+    const env = {
+      AWS_PROFILE: "my-profile",
+    };
+    const allowListStr = "";
+    configureEnvironment(env, allowListStr);
+    expect(env).toEqual({
+      AWS_ENDPOINT_URL: "http://localhost.localstack.cloud:4566",
+      AWS_ENDPOINT_URL_S3: "http://s3.localhost.localstack.cloud:4566",
+    });
+  });
+
+  test("allowlist of profile", () => {
+    const env = {
+      AWS_PROFILE: "my-profile",
+    };
+    const allowListStr = "AWS_PROFILE";
+    configureEnvironment(env, allowListStr);
+    expect(env).toEqual({
+      AWS_PROFILE: "my-profile",
+      AWS_ENDPOINT_URL: "http://localhost.localstack.cloud:4566",
+      AWS_ENDPOINT_URL_S3: "http://s3.localhost.localstack.cloud:4566",
+    });
   });
 });
